0b935d676995f163b9b42a3ba7c4f9a8102eff5a
Javi Mollà <jmolla31@gmail.com>
2019-04-01 13:23:40 +0200

    Added new "noPointer" style registration (see commit description)
    Refactored code
    
    Added a new method of registering services (doesn't make much sense but I had the idea so...).
    This can fail due to optimizations and inlining at compile time (even with the NoInlining attribute) so use it at your own risk.

--- Autoregister/Autoregister.cs
+++ Autoregister/Autoregister.cs
@@ -1,26 +1,40 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace Autoregister
 {
     public static class Autoregister
     {
-        public static void AutoregisterServices<TInterface, TPointer>(this IServiceCollection serviceColletion, ServiceLifetime serviceLifetime)
+        public static void AutoregisterServices<TInterface, TPointer>(this IServiceCollection serviceColletion, ServiceLifetime serviceLifetime = ServiceLifetime.Scoped)
             where TPointer : class
             where TInterface : class
+        => AutoregisterCore(serviceColletion, typeof(TPointer), typeof(TInterface), serviceLifetime);
+
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void AutoregisterServices(this IServiceCollection serviceColletion, ServiceLifetime serviceLifetime = ServiceLifetime.Scoped)
         {
-            var pointerType = typeof(TPointer);
-            var interfacePointerType = typeof(TInterface);
+            var assembly = Assembly.GetCallingAssembly();
 
-            var servicesAssembly = pointerType.Assembly;
+            var dualPointerType = assembly.ExportedTypes.First(typ => typ.Name.Equals("IAutoPointer"));
 
-            var services = servicesAssembly.ExportedTypes.Where(t => interfacePointerType.IsAssignableFrom(t)  && !t.IsAbstract);
+            AutoregisterCore(serviceColletion, dualPointerType, dualPointerType, serviceLifetime);
+        }
+
+        private static void AutoregisterCore(IServiceCollection serviceColletion, Type pointer, Type iPointer, ServiceLifetime lifetime)
+        {
+            var servicesAssembly = pointer.Assembly;
+
+            var services = servicesAssembly.ExportedTypes.Where(t => iPointer.IsAssignableFrom(t) && !t.IsAbstract);
 
             foreach (var service in services)
             {
-                var @interface = service.GetInterfaces().FirstOrDefault(i => interfacePointerType.IsAssignableFrom(i) && i != interfacePointerType);
+                var @interface = service.GetInterfaces().FirstOrDefault(i => iPointer.IsAssignableFrom(i) && i != iPointer);
 
-                if (@interface != null) serviceColletion.Add(new ServiceDescriptor(@interface, service, serviceLifetime));
+                if (@interface != null) serviceColletion.Add(new ServiceDescriptor(@interface, service, lifetime));
             }
         }
     }
